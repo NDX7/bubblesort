18eaea5849639be27bfbd005096723c17d49c8d6
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2024-04-07 12:06:27 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,15 +1,17 @@
+#this code has the ability to coduct bubble sort algorithm on int values only
 print("enter no. of values to be inputed")
 n=int(input())
 
 l=[]
-print("enter elements in array\n :")
+print("enter elements in array:\n")
 for i in range(0,n):
     ele=input()
     l.append(ele)
-print("unsorted elements are",l)
-
-for i in range(0,n):
-    temp = l[i]
-    l[i] = l[i+1]
-    l[i+1] = temp
+print("unsorted elements are\n",l)
+for j in range(0,n-1):
+    for i in range(0,n-1):
+        if(l[i]>l[i+1]):
+            temp = l[i]
+            l[i] = l[i+1]
+            l[i+1] = temp
 print("sorted array is:\n",l)
